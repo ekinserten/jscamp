d4ec0498306f74a889e57dfdddb5a4b6f222c4c2
ekinserten <ekinserten@gmail.com>
2021-10-19 01:46:13 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,31 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+//
+{
+    let dolarDun= 9.10
+}
+
+console.log(dolarDun)
+
+// const=sabit
+const euroDun = 11.2
+
+//euroDun=11 Hatalı yazım çünkü sabit olarak atadık.
+
+console.log(euroDun)
+
+//array=dizi demektir.
+//camelCasing
+//PascalCasing
+let konutKredileri=["Konut kredisi","Emlak konut kredisi","Kamu konut kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>")
+
+}
+console.log("</ul>")
+
